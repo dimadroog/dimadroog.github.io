421e0cccc6b72b864cd1fa3d8d141a32370253c3
DAGorbachev <DAGorbachev@1cbit.ru>
2025-08-12 10:04:23 +0300

    lisp

---
+++ lispublica-layout/README.md
@@ -0,0 +1,6 @@
+# A Starter Gulp Template
+
+## How to use
+- clone this repo
+- type ```$ npm install ``` for install plugins
+- type ```$ npm run gulp ``` for run
---
+++ lispublica-layout/tailwind.config.js
@@ -0,0 +1,87 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.njk",
+    "./app/**/*.js",
+  ],
+  darkMode: 'class', // смена темы через класс .dark
+  theme: {
+    colors: {
+      primary: {
+        50: "var(--color-primary-50)",
+        100: "var(--color-primary-100)",
+        200: "var(--color-primary-200)",
+        300: "var(--color-primary-300)",
+        400: "var(--color-primary-400)",
+        500: "var(--color-primary-500)",
+        600: "var(--color-primary-600)",
+        700: "var(--color-primary-700)",
+        800: "var(--color-primary-800)",
+        900: "var(--color-primary-900)",
+        950: "var(--color-primary-950)",
+      },
+      secondary: {
+        50: "var(--color-secondary-50)",
+        100: "var(--color-secondary-100)",
+        200: "var(--color-secondary-200)",
+        300: "var(--color-secondary-300)",
+        400: "var(--color-secondary-400)",
+        500: "var(--color-secondary-500)",
+        600: "var(--color-secondary-600)",
+        700: "var(--color-secondary-700)",
+        800: "var(--color-secondary-800)",
+        900: "var(--color-secondary-900)",
+        950: "var(--color-secondary-950)",
+      },
+      tertiary: {
+        50: "var(--color-tertiary-50)",
+        100: "var(--color-tertiary-100)",
+        200: "var(--color-tertiary-200)",
+        300: "var(--color-tertiary-300)",
+        400: "var(--color-tertiary-400)",
+        500: "var(--color-tertiary-500)",
+        600: "var(--color-tertiary-600)",
+        700: "var(--color-tertiary-700)",
+        800: "var(--color-tertiary-800)",
+        900: "var(--color-tertiary-900)",
+        950: "var(--color-tertiary-950)",
+      },
+      error: {
+        50: "var(--color-error-50)",
+        100: "var(--color-error-100)",
+        200: "var(--color-error-200)",
+        300: "var(--color-error-300)",
+        400: "var(--color-error-400)",
+        500: "var(--color-error-500)",
+        600: "var(--color-error-600)",
+        700: "var(--color-error-700)",
+        800: "var(--color-error-800)",
+        900: "var(--color-error-900)",
+        950: "var(--color-error-950)",
+      },
+      success: {
+        50: "var(--color-success-50)",
+        100: "var(--color-success-100)",
+        200: "var(--color-success-200)",
+        300: "var(--color-success-300)",
+        400: "var(--color-success-400)",
+        500: "var(--color-success-500)",
+        600: "var(--color-success-600)",
+        700: "var(--color-success-700)",
+        800: "var(--color-success-800)",
+        900: "var(--color-success-900)",
+        950: "var(--color-success-950)",
+      },
+      // Можно добавить стандартные цвета Tailwind, если нужно (например, для border/white/black):
+      white: "#fff",
+      black: "#000",
+      transparent: "transparent",
+      current: "currentColor",
+      // ...и так далее
+    },
+    extend: {
+      // другие кастомизации если нужны
+    },
+  },
+  plugins: [],
+}
