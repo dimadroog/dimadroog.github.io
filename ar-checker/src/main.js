4382fcd89584e8f04a05e2d508427ff51f324737
DAGorbachev <DAGorbachev@1cbit.ru>
2026-06-02 15:43:10 +0300

    ar-checker

--- ar-checker/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './assets/tailwind.css'
-import { initTheme } from './composables/useTheme'
-import App from './App.vue'
-
-initTheme()
-createApp(App).mount('#app')
--- ar-checker/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-// GitHub Pages (project site): https://<user>.github.io/ar-checker/
-// Относительные пути работают и в подпапке репозитория, и на корневом user.github.io
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-})
